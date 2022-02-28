de2bcf9d933562db3abb3f37900171bcc9c0165f
praveenaminnikanti55 <96469057+praveenaminnikanti55@users.noreply.github.com>
2022-02-28 11:39:30 +0530

    Update fetsch2.js

--- fetsch2.js
+++ fetsch2.js
@@ -1,42 +1,3 @@
-// const resultElement = document.getElementById('result');
-// const buttonsElement = document.getElementById('buttons');
-
-// function button(){
-//     const getData = () => {
-//         fetch("https://jsonplaceholder.typicode.com/posts")
-//         .then((response) => response.json())
-//         .then(data =>{
-//             receive(data);
-//             console.log(data);
-//         })
-//         .catch((err)=>{
-//             console.log(err);
-//         })
-//     }
-//     getData();
-// }
-
-// const receive = (data) =>{
-//     for(let i=0; i<data.length; i++){
-//         let idElement = document.createElement('p')
-//         idElement.textContent = data[i].id;
-//         resultElement.appendChild(idElement);
-
-//         let headElement = document.createElement('h3');
-//         headElement.textContent = data[i].title;
-//         resultElement.appendChild(headElement);
-        
-//         let bodyElement = document.createElement('h2');
-//         bodyElement.textContent = data[i].body;
-//         resultElement.appendChild(bodyElement);
-
-//         let hrline = document.createElement('hr');
-//         resultElement.appendChild(hrline);
-//     }
-// }
-
-
-
 const userElement = document.getElementById("userResult");
 
 
@@ -44,68 +5,78 @@ const userElement = document.getElementById("userResult");
 
 
 
-
-
-
-
 function show(datas){
-    let tableElement = document.createElement('TABLE')
+    let tableElement = document.createElement("TABLE")
     userElement.appendChild(tableElement);
 
-    let trElement = document.createElement('TR');
-    tableElement.appendChild(trElement);
-
-    let thElement = document.createElement('TH');
-    thElement.textContent = "S.NO";
+    let  trElement = document.createElement("TR");
+    tableElement.appendChild(trElement); 
+    
+    let thElement = document.createElement("TH");
+    thElement.textContent = "S.No";
     trElement.appendChild(thElement);
 
-    let thElement1 = document.createElement('TH');
-    thElement1.textContent = "Name";
+    let thElement1 = document.createElement("TH");
+    thElement.textContent = "Name";
     trElement.appendChild(thElement1);
 
-    let thElement2 = document.createElement('TH');
+    let thElement2 = document.createElement("TH");
     thElement2.textContent = "user name";
     trElement.appendChild(thElement2);
 
-    let thElement3 = document.createElement('TH');
+    let thElement3 = document.createElement("TH");
     thElement3.textContent = "email";
     trElement.appendChild(thElement3);
 
-    let thElement4 = document.createElement('TH');
+
+ let thElement4 = document.createElement("TH");
     thElement4.textContent = "phone";
     trElement.appendChild(thElement4);
 
-    let thElement5 = document.createElement('TH');
-    thElement5.textContent = "Website";
+
+let thElement5 = document.createElement("TH");
+    thElement5.textContent = "website";
     trElement.appendChild(thElement5);
 
-    for(let i=0; i<datas.length; i++){
-        let trElement1 = document.createElement("TR");
-        tableElement.appendChild(trElement1);
-        let tdEle = document.createElement("TD");
-        let tdElement = document.createElement("TD");
-        let tdElement2 = document.createElement("TD");
-        let tdElement1 = document.createElement("TD");
-        let tdElement3 = document.createElement("TD");
-        let tdElement4 = document.createElement("TD");
+   for(i=0; i<datas.length;i++){
+       let trElement1 = document.createElement("TR");
+      tableElement.appendChild(trElement1);
+      let tdEle = document.createElement("TD");
+      tdEle.textContent=datas[i].id;
+      trElement1.appendChild(tdEle);
+      let tdElement = document.createElement("TD");
+      tdElement.textContent=datas[i].name;
+      trElement1.appendChild(tdElement);
 
-        tdEle.textContent=datas[i].id;
-        tdElement.textContent = datas[i].name;
-        tdElement1.textContent = datas[i].username;
-        tdElement2.textContent=datas[i].email;
-        tdElement3.textContent=datas[i].phone;
-        tdElement4.textContent=datas[i].website;
+      let tdElement1 = document.createElement("TD");
+      tdElement1.textContent=datas[i].username;
+      trElement1.appendChild(tdElement1);
 
-        trElement1.appendChild(tdEle);
-        trElement1.appendChild(tdElement);
-        trElement1.appendChild(tdElement1);
-        trElement1.appendChild(tdElement2);
-        trElement1.appendChild(tdElement3);
-        trElement1.appendChild(tdElement4);
-    }  
-}  
+      let tdElement2 = document.createElement("TD");
+      tdElement2.textContent=datas[i].email;
+      trElement1.appendChild(tdElement2);
+
+      let tdElement3 = document.createElement("TD");
+      tdElement3.textContent=datas[i].phone;
+      trElement1.appendChild(tdElement3);
+
+      let tdElement4 = document.createElement("TD");
+      tdElement4.textContent=datas[i].website;
+      trElement1.appendChild(tdElement4);
+     
+      
+     
+
+    
+    
+     
+   } 
+
+
+} 
 
 function clicks(){
+
 const get_data=()=>{
     fetch("https://jsonplaceholder.typicode.com/users")
     .then((result)=>result.json())
@@ -119,8 +90,6 @@ const get_data=()=>{
     })
 }
 
-
 get_data();
 
 }
-
